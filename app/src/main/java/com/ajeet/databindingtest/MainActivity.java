00d2ea92b2a8577530770d86ef2fcbcdbd5e06cd
ajeetsinghpachahara <ajeet.pachahara4u@gmail.com>
2017-02-14 22:25:03 +0530

    Second Commit

--- app/src/main/java/com/ajeet/databindingtest/MainActivity.java
+++ app/src/main/java/com/ajeet/databindingtest/MainActivity.java
@@ -1,14 +1,20 @@
 package com.ajeet.databindingtest;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ajeet.databindingtest.databinding.ActivityMainBinding;
+import com.ajeet.databindingtest.model.User;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("test");
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("Test", "User");
+        binding.setUser(user);
+
     }
 }
---
+++ app/src/main/java/com/ajeet/databindingtest/model/User.java
@@ -0,0 +1,30 @@
+package com.ajeet.databindingtest.model;
+
+/**
+ * Created by Ajeet on 14-02-2017.
+ */
+
+public class User {
+    private String firstName, lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
